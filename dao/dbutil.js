502fc612bb8b8c15489c87694ecc8354a6418b37
hhf1229 <1322408331@qq.com>
2019-12-27 10:12:06 +0800

    mysql

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 function createConnection(){
   const connection = mysql.createConnection({
-    host:'127.0.0.1',
+    host:'192.168.0.102',
     port:'3306',
     user:'root',
     password:'root',
